530c4fa0f5b70bd30201ca6136112ba38bf6bb6b
Aarons <aarons@axrail.com>
2024-01-24 17:17:31 +0800

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,16 @@ import watermelon from "./assets/watermelon.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={watermelon} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Watermelon</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <p className="read-the-docs">Welcome to watermelon</p>
     </>
   );
 }
